a80a778dbd82db351c73682292ae84b3662ac727
Kelvin <kmartinez7958@gmail.com>
2023-05-07 15:08:03 -0700

    Each Button sends out distinctive strings

--- communication.py
+++ communication.py
@@ -5,20 +5,19 @@ import time
 arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
 arduino.flush()
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
+def button_state(value):
+    match value:
+        case '100':
+            print("Received value:", value)
+        case '111':
+            print("Received value:", value)
+        case _:
+            print("Invaliud")
 
-# Some things to do
-# Whem Game receives a button press, for starters let it show as a print
 while True:
 
 # Whenever a interrupt is triggered, it will them get the pre-assigned int and then check if nonempty to print
     value_str = arduino.readline().decode('utf-8').rstrip()
-
-    if(value_str):
-        print("Received value:", value_str)
-    
+    if (value_str):
+        button_state(value_str)
     
