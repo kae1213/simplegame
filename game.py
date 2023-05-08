7df3ded884c7e1875103ccaf3ffb72c5f9001677
Kelvin <kmartinez7958@gmail.com>
2023-05-07 23:27:18 -0700

    Addition for serial to game

--- game.py
+++ game.py
@@ -1,12 +1,18 @@
 
 import pygame
 import random
+import communication
+import serial
 
 # Initialize Pygame
 #just a new comment-iadded
 pygame.init()
+#communication.enable_controller()
 
 # window frame for game containment
+value_str = ''
+left_button_pressed = False
+right_button_pressed = False
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -38,22 +44,32 @@ pause_font = pygame.font.Font(None, 42)
 # Set up the game loop
 is_game_running = True
 clock = pygame.time.Clock()
+arduino1 = communication.connect_to_controller()
+#arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
+#arduino.flush()
 
 while is_game_running:
     # Handle events
+    value_str = arduino1.readline().decode('utf-8').rstrip()
+    print(type(value_str))
+    if (value_str):
+        communication.button_state(value_str, left_button_pressed, right_button_pressed)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:#only event to handle is quit
             is_game_running = False
-        # Pause Game
-        # New Game
-        # Lost Game
 
     # Controls for player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_1_x > 0:
+    #keys = pygame.key.get_pressed()
+    #if keys[pygame.K_LEFT] and player_1_x > 0:
+    if communication.left_button_pressed and player_1_x > 0:
         player_1_x -= player_1_speed
-    if keys[pygame.K_RIGHT] and player_1_x < screen_width - player_1_width:
+        communication.left_button_pressed = False
+        
+    #if keys[pygame.K_RIGHT] and player_1_x < screen_width - player_1_width:
+    if communication.right_button_pressed and player_1_x > 0:
         player_1_x += player_1_speed
+        communication.right_button_pressed = False
 
     # Moving the enemy
     enemy_1_y += enemy_1_speed
@@ -75,8 +91,14 @@ while is_game_running:
     screen.blit(pause_text, (765, 10))
     pygame.display.flip()
 
+    
+
     # Frame of game 60hz
     clock.tick(60)
 
 # System Clear
 pygame.quit()
+
+# value_str = arduino.readline().decode('utf-8').rstrip()
+#     if (value_str):
+#         communication.button_state(value_str)
